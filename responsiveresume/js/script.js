9480e036ace16b9518d17f36ee9ee96fee3d31ce
Amanpreet Kaur <er.amansohal@gmail.com>
2017-01-26 16:37:19 +0300

    Add files via upload

---
+++ responsiveresume/js/script.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function(){
+	$("#progress-bar1").animate({
+    	width: "90%"
+	}, 2500);
+	$("#progress-bar2").animate({
+    	width: "80%"
+	}, 3000);
+	$("#progress-bar3").animate({
+    	width: "75%"
+	}, 3500);
+	$("#progress-bar4").animate({
+    	width: "70%"
+	}, 4000);
+	$("#progress-bar5").animate({
+    	width: "70%"
+	}, 4500);
+	$("#progress-bar6").animate({
+    	width: "80%"
+	}, 5000);
+	$("#progress-bar7").animate({
+    	width: "65%"
+	}, 5500);
+	$("#progress-bar8").animate({
+    	width: "85%"
+	}, 6000);
+	$("#progress-bar9").animate({
+    	width: "85%"
+	}, 6500);
+	
+})
